cc44a826fc7ed7bfe3fddbdb09052d4349222bb7
lemonssssaa <3118130399@qq.com>
2021-05-28 19:41:46 +0800

    test

--- src/main/java/com/panshi/filter/GlobalConfigFilter.java
+++ src/main/java/com/panshi/filter/GlobalConfigFilter.java
@@ -11,11 +11,21 @@ import java.io.IOException;
 public class GlobalConfigFilter implements Filter {
 
     @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest req = (HttpServletRequest) servletRequest;
         HttpServletResponse resp = (HttpServletResponse) servletResponse;
         req.setCharacterEncoding("UTF-8");
         resp.setCharacterEncoding("UTF-8");
         filterChain.doFilter(req,resp);
+    }
+
+    @Override
+    public void destroy() {
+
     }
 }
